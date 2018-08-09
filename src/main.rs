18c20c079681d1e56735dd8887c46e6b75f4526b
Anton Filippov <a.filippov@protonmail.com>
2018-08-09 13:14:20 +0700

    More, better, cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,86 @@
 #![feature(rust_2018_preview)]
-use std::{path::Path, fs, io::prelude::*};
 use clap::clap_app;
+use std::{fs, path::Path};
 
-fn main() {
-    let matches = clap_app!(blogg =>    
+fn main() -> std::io::Result<()> {
+    let matches = clap_app!(blogg =>
         (version: "0.1.0")
         (author: "Anton F. <a.filippov@protonmail.com>")
         (about: "Static blog generator")
-        (@arg open: -o --open "Opens generated index file in default browser on completion")
+        (@arg open: -o --open "Opens generated index file in default browser upon completion")
+        (@arg SOURCE: --src <SOURCE> "Sets path to directory with blog post sources")
+        (@arg TARGET: --tgt [TARGET] "Sets path to target directory (defaults to './blogg_target')")
     ).get_matches();
 
-    let tgt_dir = Path::new("blogg_target");
-    
-    if tgt_dir.exists() {
-        fs::remove_dir_all(tgt_dir).unwrap();
+    let src_dir = Path::new(matches.value_of("SOURCE").unwrap());
+    if !src_dir.exists() {
+        panic!("unable to locate source directory");
     }
-    fs::create_dir(tgt_dir).unwrap();
 
-    let dir = "blog_src";
+    let tgt_dir = Path::new(matches.value_of("TARGET").unwrap_or("blogg_target"));
+    if tgt_dir.exists() {
+        fs::remove_dir_all(tgt_dir)?;
+    }
+    fs::create_dir(tgt_dir)?;
 
     let mut index = String::new();
-    for e in fs::read_dir(dir).unwrap() {
-        let e = e.unwrap();
-        let mut f = fs::File::open(e.path()).unwrap();
-        let mut contents = String::new();
-        f.read_to_string(&mut contents).unwrap();
+    let mut entries = fs::read_dir(src_dir)?
+        .map(Result::unwrap)
+        .map(|e| {
+            let created = fs::metadata(e.path())
+                .and_then(|meta| meta.created())
+                .unwrap();
+            (created, e)
+        }).collect::<Vec<_>>();
 
-        let out_f_path = format!("{}.html", e.path().file_stem().unwrap().to_str().unwrap());
-        let out_path = tgt_dir.join(Path::new(&out_f_path));
-        let mut out_f = fs::File::create(&out_path).unwrap();
-        out_f.write_fmt(format_args!(include!("templates/post.html"), e.file_name().to_str().unwrap(), contents)).unwrap();
+    entries.sort_by_key(|(created, _)| *created);
+    entries.reverse();
 
-        index.push_str(&format!("<li><a href=\"{}\">{}</a></li>\n", out_f_path, e.file_name().to_str().unwrap()));
+    for (created, entry) in entries {
+        let contents = fs::read_to_string(entry.path())?;
+
+        let out_f_path = entry.path().with_extension("html");
+        let out_f_filename = out_f_path.file_name().unwrap();
+
+        let out_path = tgt_dir.join(out_f_filename);
+        fs::write(
+            out_path,
+            format!(
+                include_str!("templates/post.html"),
+                entry.file_name().to_str().unwrap(),
+                contents
+            ),
+        )?;
+
+        index.push_str(&format!(
+            include_str!("templates/index_item.html"),
+            out_f_filename.to_str().unwrap(),
+            entry.file_name().to_str().unwrap(),
+            chrono::DateTime::<chrono::offset::Utc>::from(created).format("%B %e, %Y, %I:%M %p")
+        ));
     }
 
-    let mut index_f = fs::File::create(tgt_dir.join("index.html")).unwrap();
-    index_f.write_fmt(format_args!(include!("templates/index.html"), dir, index)).unwrap();
+    fs::write(
+        tgt_dir.join("index.html"),
+        format!(
+            include_str!("templates/index.html"),
+            src_dir.display(),
+            index
+        ),
+    )?;
+
+    let styles_dir = tgt_dir.join("styles");
+    fs::create_dir(&styles_dir)?;
+    fs::write(
+        styles_dir.join("styles.css"),
+        include_str!("styles/styles.css"),
+    )?;
+
     println!("Blog generated!");
-    
-    if matches.is_present("open") && open::that(tgt_dir.join("index.html")).is_ok() {
-        println!("Opening!..");
+    if matches.is_present("open") {
+        open::that(tgt_dir.join("index.html"))?;
+        println!("Opened in browser!");
     }
+
+    Ok(())
 }
